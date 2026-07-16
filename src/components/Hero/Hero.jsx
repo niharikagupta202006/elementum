04bf125f4bc4ba8f5e05931dbcfe5a9ddf9277ed
Niharika Gupta <Niharikaagupta0@gmail.com>
2026-07-16 14:17:48 +0530

    Fix person image imports

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,10 @@
 import "./Hero.css";
+import person1 from "../../assets/images/person1.jpg";
+import person2 from "../../assets/images/person2.jpg";
+import person3 from "../../assets/images/person3.jpg";
+import person4 from "../../assets/images/person4.jpg";
+import person5 from "../../assets/images/person5.jpg";
+import person6 from "../../assets/images/person6.jpg";
 
 function Hero() {
   return (
@@ -19,12 +25,12 @@ function Hero() {
       </p>
 
       <div className="people">
-  <img src="/src/assets/images/person1.jpg" alt="" />
-  <img src="/src/assets/images/person2.jpg" alt="" />
-  <img src="/src/assets/images/person3.jpg" alt="" />
-  <img src="/src/assets/images/person4.jpg" alt="" />
-  <img src="/src/assets/images/person5.jpg" alt="" />
-  <img src="/src/assets/images/person6.jpg" alt="" />
+  <img src={person1} alt="Person 1" />
+  <img src={person2} alt="Person 2" />
+  <img src={person3} alt="Person 3" />
+  <img src={person4} alt="Person 4" />
+  <img src={person5} alt="Person 5" />
+  <img src={person6} alt="Person 6" />
 </div>
 
     </section>
